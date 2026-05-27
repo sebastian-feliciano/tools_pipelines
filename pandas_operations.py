982247f65427983e91afcc03ef990e6d339897f7
Sebastian Feliciano <felicianosebastiane@gmail.com>
2026-05-26 23:27:41 -0400

    Add summarization operations for sales and users

--- pandas_operations.py
+++ pandas_operations.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 orders_df = pd.read_csv("orders.csv")
 
+# List of possible pandas summarization operations
 sum = orders_df["sales"].sum()
 count = orders_df["order_id"]
 distinctcount = orders_df["user_id"].nunique()
